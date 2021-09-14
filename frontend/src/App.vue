8cd061aaec4a8d24b05b763b4346b53b52a6f5c7
김기도 <echachca@naver.com>
2021-09-15 00:16:41 +0900

    Frontend Refactor 배포test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">배포test</router-link> |
+      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link :to="{ name: 'Main' }">Main</router-link>
     </div>
