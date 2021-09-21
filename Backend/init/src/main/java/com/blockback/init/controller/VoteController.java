27550cef011285dc96dc66bd75e8dbbe17fd926e
송지연 <6loutlside@naver.com>
2021-09-21 20:46:19 +0900

    Backend Feat / 전체 투표 조회 API 구현

--- Backend/init/src/main/java/com/blockback/init/controller/VoteController.java
+++ Backend/init/src/main/java/com/blockback/init/controller/VoteController.java
@@ -1,10 +1,15 @@
 package com.blockback.init.controller;
 
+import com.blockback.init.common.response.VoteListRes;
+import com.blockback.init.service.VoteService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -12,4 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @EnableRedisHttpSession
 public class VoteController {
+
+    @Autowired
+    VoteService voteService;
+
+    @GetMapping("/{clubid}/vote")
+    @ApiOperation(value = "투표 전체 조회", notes = "투표 전체 목록을 조회한다.")
+    public ResponseEntity<List<VoteListRes>> getVoteList(@RequestParam Long clubid) {
+        List<VoteListRes> res = voteService.getVoteList(clubid);
+        return ResponseEntity.status(200).body(res);
+    }
 }
