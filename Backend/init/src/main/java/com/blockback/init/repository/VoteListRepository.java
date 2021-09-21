21653f976dcc4f6931e121113bbb12aaef6df974
송지연 <6loutlside@naver.com>
2021-09-21 20:47:13 +0900

    Backend Feat / ID로 조회

--- Backend/init/src/main/java/com/blockback/init/repository/VoteListRepository.java
+++ Backend/init/src/main/java/com/blockback/init/repository/VoteListRepository.java
@@ -4,6 +4,9 @@ import com.blockback.init.entity.VoteList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VoteListRepository extends JpaRepository<VoteList, Long> {
+    List<VoteList> findByVoteId(Long id);
 }
--- Backend/init/src/main/java/com/blockback/init/repository/VoteRepository.java
+++ Backend/init/src/main/java/com/blockback/init/repository/VoteRepository.java
@@ -4,6 +4,9 @@ import com.blockback.init.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    List<Vote> findAllById(Long clubid);
 }
