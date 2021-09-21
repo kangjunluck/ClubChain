28c7b8f517d6a2ceb18aef3775791618fb05bf16
송지연 <6loutlside@naver.com>
2021-09-21 21:19:40 +0900

    Backend Feat / 투표 생성 구현

--- Backend/init/src/main/java/com/blockback/init/controller/VoteController.java
+++ Backend/init/src/main/java/com/blockback/init/controller/VoteController.java
@@ -1,6 +1,10 @@
 package com.blockback.init.controller;
 
+import com.blockback.init.common.request.VoteCreateReq;
+import com.blockback.init.common.response.MessageResponse;
 import com.blockback.init.common.response.VoteListRes;
+import com.blockback.init.entity.User;
+import com.blockback.init.service.UserService;
 import com.blockback.init.service.VoteService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -21,10 +26,31 @@ public class VoteController {
     @Autowired
     VoteService voteService;
 
+    @Autowired
+    UserService userService;
+
+    String SUCCESS = "Success";
+    String FAIL = "Fail";
+
     @GetMapping("/{clubid}/vote")
     @ApiOperation(value = "투표 전체 조회", notes = "투표 전체 목록을 조회한다.")
     public ResponseEntity<List<VoteListRes>> getVoteList(@RequestParam Long clubid) {
         List<VoteListRes> res = voteService.getVoteList(clubid);
         return ResponseEntity.status(200).body(res);
+    }
+
+    @PostMapping("/{clubid}/vote")
+    @ApiOperation(value = "투표 만들기", notes = "투표를 만든다.")
+    public ResponseEntity<MessageResponse> createVote(@RequestParam Long clubid, HttpSession session,
+                                                        @RequestBody VoteCreateReq req) {
+        String owner_email = (String) session.getAttribute("LoginUser");
+        // 투표 생성자 정보
+        User user = userService.getUserByUserEmail(owner_email);
+
+        if(voteService.createVote(clubid, req, user)) {
+            return ResponseEntity.status(200).body(MessageResponse.of(200, SUCCESS));
+        }
+
+        return ResponseEntity.status(500).body(MessageResponse.of(500, FAIL));
     }
 }
