4e2265382051f0396dbeaa4130c86162f01f8af6
unknown <y8s8y8@gmail.com>
2021-09-24 17:19:23 +0900

    frontedn feature/ etherium page, componenet, view add

---
+++ frontend/src/components/finance/financeinfo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="finanaceInfo">
+    <div>회비관리</div>
+    <div class="clubname">동아리 이름</div>
+    <b-container class="infomation">
+      <b-row>
+        <b-col cols="1" align-self="center">◀</b-col>
+        <b-col cols="10" class="ethCard">
+          <div>356계좌번호04SDF64SFX</div>
+          <div style="font-size: 3rem">잔고 ETH</div>
+          <div><span class="button1">거래내역</span><span class="button2">이체</span></div>
+        </b-col> 
+        <b-col cols="1" align-self="center">▶</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FinanceInfo",
+}
+</script>
+<style>
+/* 왜 scoped 하면 적용 안되지? */
+.clubname {
+  text-align: left;
+  padding: 0 0 0 9%;
+  margin: 0 0 0.1rem 0;
+  font-size: 1.5rem;
+}
+/* .infomation >>> .ethCard { */
+.ethCard {
+  background-color: #1EC0FF;
+  height: 10rem;
+  border-radius: 0.5rem;
+  color: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.button1 {
+  margin: 0 1rem 0 0;
+}
+.button2 {
+  margin: 0 0 0 1rem;
+}
+</style>
---
+++ frontend/src/views/finance.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="finance">
+    <!-- 상단바도 컴포넌트로 두고 전환해야 하나? -->
+    <FinanceInfo />
+    <!-- <v-if></v-if> -->
+    <!-- if로 컴포넌트 전환?  -->
+    <TransactionHistory />
+    <Transaction />
+    <Footer />
+    <!-- footer 어따 달지? -->
+  </div>
+</template>
+
+<script>
+import FinanceInfo from "@/components/finance/financeinfo.vue";
+import TransactionHistory from "@/components/finance/transactionHistory.vue";
+import Transaction from "@/components/finance/transaction.vue";
+import Footer from "@/components/footer/footer.vue";
+export default {
+  components: {
+    FinanceInfo,
+    TransactionHistory,
+    Transaction,
+    Footer,
+  },
+}
+</script>
+<style scoped>
+.finance {
+  height: 100%;
+}
+</style>
