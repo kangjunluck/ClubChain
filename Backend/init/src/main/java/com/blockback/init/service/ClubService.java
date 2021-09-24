45815eeed2321e5be95bfaafae374b403a81b4f7
DESKTOP-KVCQHCD\multicampus <kangjunlucky@naver.com>
2021-09-24 11:00:26 +0900

    Backend refact / conflict solve

--- Backend/init/src/main/java/com/blockback/init/service/ClubService.java
+++ Backend/init/src/main/java/com/blockback/init/service/ClubService.java
@@ -13,5 +13,7 @@ public interface ClubService {
     List<ClubListRes> getClubList();
     void createClub(User user, ClubCreatedReq req, MultipartFile clubThumbnail) throws IOException;
     List<ClubListRes> getClubListBySearch(String name);
+    boolean modifyClub(ClubCreatedReq req, MultipartFile clubThumbnail, Long clubid);
+    boolean isOwner(User user, Long clubid);
     List<ClubListRes> getSignClubList(User user);
 }
--- Backend/init/src/main/java/com/blockback/init/service/ClubServiceImpl.java
+++ Backend/init/src/main/java/com/blockback/init/service/ClubServiceImpl.java
@@ -14,6 +14,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -73,6 +76,57 @@ public class ClubServiceImpl implements ClubService {
         clubTmp.setProfile_thumbnail(getShortFilePath(path));
 
         clubRepository.save(clubTmp);
+    }
+
+    @Override
+    public boolean modifyClub(ClubCreatedReq req, MultipartFile clubThumbnail, Long clubid) {
+        Optional<Club> club = clubRepository.findById(clubid);
+        if(!club.isPresent()) {
+            return false;
+        }
+
+        club.get().setPassword(req.getPassword());
+        club.get().setIntroduce(req.getIntroduce());
+        club.get().setName(req.getName());
+
+        try {
+            String originPath = club.get().getProfile_thumbnail();
+            if(clubThumbnail != null) { // 입력이 있을 때
+                if(!originPath.equals("image/club/default.png")) { // 디폴트가 아니면 삭제해주기
+                    String dpath = BASE_PATH + originPath.substring(11);
+                    Path deleteFilePath = Paths.get(dpath);
+                    Files.deleteIfExists(deleteFilePath);
+                }
+
+                String newPath = BASE_PATH + club.get().getId() + "-" + clubThumbnail.getOriginalFilename();
+                File dest = new File(newPath);
+                clubThumbnail.transferTo(dest);
+                if(!dest.exists()) {
+                    System.out.println("파일 업로드 실패");
+                }else {
+                    club.get().setProfile_thumbnail(getShortFilePath(newPath));
+                }
+            }
+
+            clubRepository.save(club.get());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean isOwner(User user, Long clubid) {
+        Optional<Club> club = clubRepository.findById(clubid);
+        if(!club.isPresent()) {
+            return false;
+        }
+
+        if(user.getUserEmail().equals(club.get().getUser().getUserEmail())) {
+            return true;
+        }
+        return false;
     }
 
     @Override
