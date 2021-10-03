2b91415eafa8de1e4004a69f52b96cd3c68a5be3
DESKTOP-KVCQHCD\multicampus <kangjunlucky@naver.com>
2021-10-03 19:24:31 +0900

    Front css / 회원수정, 관리

--- frontend/src/views/TheMyPage.vue
+++ frontend/src/views/TheMyPage.vue
@@ -1,58 +1,74 @@
 <template>
-  <div>
-    <b-container>
-      <b-row>
-        <b-col offset="1" cols="4" class="circle">
-          <img src="" alt="" />
-        </b-col>
-        <b-col cols="6">
-          <div class="profile_info">
-            <span class="nickname">
-              {{ this.$store.state.credentials.usernickname }}
-            </span>
-            <br />
-            <span class="email">{{
-              this.$store.state.credentials.userEmail
-            }}</span>
+    <div class="container d-flex flex-column justify-content-center align-items-center">
+      <div class="row w-50">
+        <div class="h2 p-0 mb-3" style="text-align:left;">MyPage</div>
+      </div>
+      <div class="row w-50">
+        <div class="d-flex justify-content-between align-items-center mb-3 p-0">
+          <img :src="selecturl" alt="" class="px-0" style="width:3rem; height:3rem;" />
+          <div class="w-50">
+            <p class="m-0 h5">{{ this.userinfo.userEmail }}</p>
+            <p class="m-0 h5">{{ this.userinfo.usernickname }}</p>
           </div>
-        </b-col>
-      </b-row>
-    </b-container>
-    
-    <button @click="profileUpdate">프로필 설정</button>
-    <hr />
-    <div @click="myClubInfo">가입한 동호회</div>
-    <hr />
-    <div  @click="myPost">내가 쓴 글</div>
-    <hr />
-    <div @click="userDelete">회원 탈퇴</div>
-  </div>
+        </div>
+        <button @click="profileUpdate" class="btn update-button w-50">설정</button>
+      </div>
+      <hr size="1px" width="60%">
+      <div class="row w-50">
+        <div class="p-0 mb-1" style="text-align:left;" @click="myClubInfo">가입한 동호회</div>
+        <div v-for="club in myclubinfo" :key="club.pk">
+          <p class="mb-1">{{club.introduce}}</p>
+        </div>
+      </div>
+      <hr>
+      <div class="row w-50">
+        <button @click="userDelete" class="btn delete-button w-50">탈퇴</button>
+      </div>
+    </div>
 </template>
 
 <script>
 import http from "@/util/http-common";
 export default {
   name: "myInfoPage",
+  data: function () {
+    return {
+      userinfo : null,
+      myclubinfo : null,
+
+      selecturl: require("../assets/profile.png") 
+    }
+  },
   methods: {
+    // 모든 vue에서 실행될 checklogin 함수 #######
+    checkLogin() {
+      http
+        .get("api/users/islogin",{ withCredentials: true })
+        .then((res) => {
+          this.userinfo = res.data;
+          this.myClubInfo();
+        })
+        .catch((error)=>{
+          console.log(error)
+          this.$router.push('Main')
+        })
+    },
     profileUpdate() {
       console.log('프로필 업뎃')
       this.$router.push('myinfoupdate')
     },
     myClubInfo() {
       console.log('가입한 동호회')
-  
       http
-        .post("api/club/myclub",{ withCredentials: true })
+        .get("api/club/myclub",{ withCredentials: true })
         .then((res) => {
-          console.log(res)
+          console.log(res.data)
+          this.myclubinfo = res.data;
         })
         .catch((error)=>{
           console.log('에러')
           console.log(error)
         })
-    },
-    myPost() {
-      console.log('내 포스팅')
     },
     userDelete() {
       console.log('탈퇴하기')
@@ -75,6 +91,9 @@ export default {
           console.log(error)
         })
     }
+  },
+  created () {
+    this.checkLogin();
   }
 };
 </script>
@@ -102,5 +121,21 @@ export default {
   color: red;
   margin: 0 0 0 0;
   padding: 0 0 0 0;
+}
+.update-button{
+  margin-left: auto;
+  padding: 3px 15px;
+  background-color:#1ec0ff;
+  border-radius: 2px;
+  color: white;
+  cursor: pointer;
+}
+.delete-button{
+  margin-left: auto;
+  padding: 3px 15px;
+  background-color:#ffc5c5;
+  border-radius: 2px;
+  color: rgb(255, 10, 10);
+  cursor: pointer;
 }
 </style>
