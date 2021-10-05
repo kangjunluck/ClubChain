e5dfed981cc5c3cd0f798b6364c250d4d10797f6
DESKTOP-KVCQHCD\multicampus <kangjunlucky@naver.com>
2021-10-05 14:24:47 +0900

    Front add / 회원가입 수정, route 변경

--- frontend/src/views/TheMyPage.vue
+++ frontend/src/views/TheMyPage.vue
@@ -67,7 +67,7 @@ export default {
         })
         .catch((error) => {
           console.log(error);
-          this.$router.push("Main");
+          this.$router.push("/");
         });
     },
     profileUpdate() {
