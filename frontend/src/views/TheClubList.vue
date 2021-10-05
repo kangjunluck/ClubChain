685e2ed60b6b20b10eb08cbb5217db11739c5c4b
Jiyeon526 <b8480@naver.com>
2021-10-06 07:28:09 +0900

    Frontend Fix / 동호회 썸네일

--- frontend/src/views/TheClubList.vue
+++ frontend/src/views/TheClubList.vue
@@ -55,7 +55,7 @@
           <!-- 전체 동호회 리스트 -->
           <div class="clubarea mt-2" >
             <div v-for="club in totalclublist" v-bind:key="club" class="club">
-              <img :src="'resources/' + club.profile_thumbnail" class="img-style" alt="클럽썸네일" @click="enterClub(club.clubid)">
+              <img :src="'resources/image/club/default.png'" class="img-style" alt="클럽썸네일" @click="enterClub(club.clubid)">
               <!--club 안에 썸네일 주소를 통해 이미지 불러와야함-->
               <div @click="enterClub(club.clubid)" class="club-name-style">
               {{club.name}}
