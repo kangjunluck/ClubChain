40b5d4a518540cd7078fec03a6b27fbbda3d95a6
DESKTOP-KVCQHCD\multicampus <kangjunlucky@naver.com>
2021-10-07 03:03:16 +0900

    BASEURL 수정

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // axios 객체 생성
 // 서버 돌아가는 주소값 입력
 export default axios.create({
-  baseURL: "http://j5b1021.p.ssafy.io/:8080/",
+  baseURL: "http://j5b1021.p.ssafy.io:8080/",
   headers: {
     "Content-type": "application/json",
   },
