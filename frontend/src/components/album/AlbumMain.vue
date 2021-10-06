0c12da324cbf78a583d4cbaccab0586196fd6618
y8s8y8 <y8s8y8@gmail.com>
2021-10-06 16:29:46 +0900

    frontend fix / album res data 형식 변경

--- frontend/src/components/album/AlbumMain.vue
+++ frontend/src/components/album/AlbumMain.vue
@@ -54,10 +54,10 @@ export default {
         .get("api/" + this.clubId + "/board/photo")
         .then((res) => {
           console.log(res);
-          console.log(res.total, "전체사진");
-          console.log("리스트", res.list);
-          this.photoCount = res.total;
-          this.photoList = res.list;
+          console.log(res.data.total, "전체사진");
+          console.log("리스트", res.data.list);
+          this.photoCount = res.data.total;
+          this.photoList = res.data.list;
         })
         .catch((error) => {
           console.log(error);
