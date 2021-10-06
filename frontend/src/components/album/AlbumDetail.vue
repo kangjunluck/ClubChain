8b41c84d291075183379e6c7a34e5577b9581ac5
unknown <y8s8y8@gmail.com>
2021-10-06 23:08:17 +0900

    frontend fix / album detail2

--- frontend/src/components/album/AlbumDetail.vue
+++ frontend/src/components/album/AlbumDetail.vue
@@ -1,41 +1,67 @@
 <template>
   <div class="box">
-    <!-- <div>앨범 상세 페이지</div> -->
-    <!-- <div>{{photoIndex}}</div> -->
+
     <div class="image_area">
-      <img class="image" :src="'/resources/'+photo.photo_address" alt="상세 이미지">
-      
-      <!-- <img class="image" src="@/assets/cats.jpg" alt="이미지"> -->
+      <!-- <img class="image" :src="'/resources/'+photo.photo_address" alt="상세 이미지"> -->
+      <img class="image" src="@/assets/근데.png" alt="이미지">
     </div>
-    <div>하단</div>
-    <div>{{photo}}</div>
+    <div>
+      <div>야근 중</div>
+      <div>2021-10-06 07:54:09</div>
+      <!-- <div>{{photo.user_name}}</div>
+      <div>{{photo.time}}</div> -->
+    </div>
+
+    <!-- <div>{{photo}}</div>
       <div>{{photo.photo_address}}</div>
       <div>{{photo.time}}</div>
       <div>{{photo.user_name}}</div>
-      <div>{{index}}</div>
+      <div>{{index}}</div> -->
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      afterDayFormat:"",
+    }
+  },
   props: ["photo", "index"],
+  monunted: {
+    theDay(photo) {
+      console.log('파싱 전', photo.time)
+      var parsingdata1 = photo.time.substring(0,10)
+      var parsingdata2 = parsingdata1.replace('-', '년 ')
+      var parsingdata3 = parsingdata2.replace('-', '월 ')
+      this.afterDayFormat = parsingdata3 + "일"
+      console.log(this.afterDayFormat)
+
+      
+    }
+  },
+
 };
 </script>
 
 <style scoped>
 .box{
   width:100%;
-  min-height:50%;
+  min-height:80%;
 }
-.image_area{
+/* .image_area{
   background-color:aquamarine;
   width:100%;
   height:100vw;
   overflow:hidden;
-  /* margin:0 auto; */
   margin: 0 0 0 0;
-}
+} */
 .image{
-  object-fit:cover;
+  /* width: 200px;
+  height: 200px; */
+  width: 100%;
+  height: 150vw;
+  object-fit: contain;
+  border: 1px solid #000    
 }
 </style>
