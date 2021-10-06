7944c1ac973b5d2be08b08653bbef1f7ce120b06
unknown <y8s8y8@gmail.com>
2021-10-06 23:23:03 +0900

    frontend fix / album detail

--- frontend/src/components/album/AlbumDetail.vue
+++ frontend/src/components/album/AlbumDetail.vue
@@ -8,8 +8,8 @@
     <div>
       <div>야근 중</div>
       <div>2021-10-06 07:54:09</div>
-      <!-- <div>{{photo.user_name}}</div>
-      <div>{{photo.time}}</div> -->
+      <div>{{photo.user_name}}</div>
+      <div>{{afterDayFormat}}</div>
     </div>
 
     <!-- <div>{{photo}}</div>
@@ -25,21 +25,38 @@ export default {
   data(){
     return {
       afterDayFormat:"",
+      // photo: photo,
     }
   },
   props: ["photo", "index"],
-  monunted: {
-    theDay(photo) {
+  methods: {
+    parsingDay(photo){
       console.log('파싱 전', photo.time)
       var parsingdata1 = photo.time.substring(0,10)
       var parsingdata2 = parsingdata1.replace('-', '년 ')
       var parsingdata3 = parsingdata2.replace('-', '월 ')
       this.afterDayFormat = parsingdata3 + "일"
       console.log(this.afterDayFormat)
-
-      
-    }
+    },
+    parsingDay0(photo){
+      console.log('파싱 전', photo.time)
+      var parsingdata1 = photo.time.substring(0,10)
+      var parsingdata2 = parsingdata1.replace('-', '년 ')
+      var parsingdata3 = parsingdata2.replace('-', '월 ')
+      this.afterDayFormat = parsingdata3 + "일"
+      console.log(this.afterDayFormat)
+    },
   },
+  monunted() {
+    console.log('마운트 시작')
+    this.parsingDay(this.photo)
+    console.log('마운트 끝')
+  },
+  created() {
+    console.log('created 시작')
+    this.parsingDay0(this.photo)
+    console.log('created 끝')
+  }
 
 };
 </script>
